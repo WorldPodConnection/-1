b7be489e6f6310a043e454d4d72b0aed1d6a2cb9
WorldPodConnection <73381042+WorldPodConnection@users.noreply.github.com>
2020-10-25 08:39:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    
+    <link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="stylesheet/sass/index.css" type="text/css" rel="stylesheet">
+    <link href="stylesheet/sass/header.css" type="text/css" rel="stylesheet">
+    <link href="stylesheet/sass/section.css" type="text/css" rel="stylesheet">
+    <link href="stylesheet/sass/footer.css" type="text/css" rel="stylesheet">
+    <title>World Pod Connection</title>
+</head>
+<body>
+  <?php include('./stylesheet/header.php'); ?>
+  <script type="text/javascript" src="stylesheet/js/toggle.js"></script>
+  <script type="text/javascript" src="stylesheet/js/include.js"></script>
+    <section class="section">
+      <div class="block first">
+        
+        <div class="img"></div>
+        <p>World Pod Connection ha come obbiettivo quello di raccogliere storie uniche e personali da tutto il mondo e depositarle all'interno di una capsula del tempo virtuale.</p>
+        <div>
+        <button onclick="location.href='partecipa/partecipa.php'" type="button">PARTECIPA ORA</button>
+        </div>
+      </div>
+        
+      <hr>
+
+
+      <h1 class="title">COME FUNZIONA?</h1>
+
+      <div class="des">
+ 
+        <div class="block second step one">
+          
+        </div>
+
+        <div class="block second step two">
+          
+
+        </div>
+
+        
+        
+
+      </div>
+      
+      <div class="des">
+ 
+      <div class="block second step three">
+          
+        </div>
+
+        <div class="block second step four">
+          
+        </div>
+
+      </div>
+
+      <hr>
+
+      <div class="block third">
+        <h1 class="title">Connessione</h1>
+        <p>Siamo nodi di una rete di scambi, nella quale ci passiamo immagini, strumenti, informazioni e conoscenza.<br> Ma del mondo che vediamo siamo anche parte integrante, non siamo osservatori esterni. <br>Siamo situati in esso.<br> La nostra prospettiva su di esso è dall’interno. Siamo fatti degli stessi atomi e degli stessi segnali di luce che si scambiano i pini sulle montagne e le stelle nelle galassie.</p>
+      </div>
+    </section>
+  <?php include('./stylesheet/footer.php'); ?>
+</body>
+</html>
